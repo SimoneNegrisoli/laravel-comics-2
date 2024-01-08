ab91859cb5826914f433bedfffeea2762085782f
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-08 20:44:38 +0100

    seeding db

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = config('db.comics');
+        //dd($data);
+
+        foreach ($data as $el) {
+            $new_comic = new Comic();
+            $new_comic->title = $el['title'];
+            $new_comic->description = $el['description'];
+            $new_comic->thumb = $el['thumb'];
+            $new_comic->price = $el['price'];
+            $new_comic->sale_date = $el['sale_date'];
+            $new_comic->series = $el['series'];
+            $new_comic->type = $el['type'];
+            $new_comic->save();
+        }
     }
 }
